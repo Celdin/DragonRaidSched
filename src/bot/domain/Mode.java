96d0d1be94e8eb992f2706654e42413741977480
Choya Deathmortus <51953764+Deathmortus@users.noreply.github.com>
2019-06-24 13:26:29 +0200

    Update Mode.java

--- src/bot/domain/Mode.java
+++ src/bot/domain/Mode.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum Mode {
-    INFANTILE("Infantile", "https://cdn.discordapp.com/attachments/426784911731458048/432967871623200770/Icone_evenement_crane.png"),
-    TRIBULATION("Tribulation", "https://cdn.discordapp.com/attachments/426784911731458048/432967869861593098/Icone_crane_rouge.png");
+    INFANTILE("Infantile : Je suis votre meilleur ami !", "https://cdn.discordapp.com/attachments/426784911731458048/432967871623200770/Icone_evenement_crane.png"),
+    TRIBULATION("Tribulation : Un grand pouvoir implique de grandes responsabilités !", "https://cdn.discordapp.com/attachments/426784911731458048/432967869861593098/Icone_crane_rouge.png");
 
     private String nom;
     private String url;
